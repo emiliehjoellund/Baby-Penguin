bb0c10f81708a98c3231bc2617a7cb10a00e77c5
emiliehjoellund <mail@emiliehjoellund.dk>
2024-04-19 12:54:52 +0200

    GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int[] fishCount = new int[4];
+    private int[] playerPosition = new int[4];
+    private int fishInPond = 12;
+
+    public void AddFishToPond(int amount)
     {
-        
+        fishInPond += amount;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void RemoveFishFromPond(int amount)
     {
-        
+        fishInPond -= amount;
     }
+
+    public void AddFishToPlayer(int player, int amount)
+    {
+        fishCount[player] += amount;
+    }
+
+    public void RemoveFishFromPlayer(int player, int amount)
+    {
+        fishCount[player] -= amount;
+    }
+
+    public List<Spaces> Spaces = new List<Spaces>();
+
+
+
 }
